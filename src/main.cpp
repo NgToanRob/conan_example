d1f28ed3586cc6c1aaa15908441d4582b1e2fe5e
Toan Nguyen <nguyentoan10610@gmail.com>
2024-01-08 15:01:24 +0300

    use only cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,46 @@
-#include <iostream>
 #include <opencv2/opencv.hpp>
+#include <iostream>
+#include <chrono>
 
 int main() {
-    std::string imagePath = "Do_yeu.jpg";
+    auto start = std::chrono::high_resolution_clock::now(); // Bắt đầu đo thời gian
+    // Đường dẫn tới ảnh
+    std::string imagePath = "../Do_yeu.jpg";
 
-    cv::Mat originalImage = cv::imread(imagePath);
-    if (originalImage.empty()) {
-        std::cout << "Error: Unable to load image!" << std::endl;
-        return 1;
-    }
+    // Đọc ảnh từ đường dẫn
+    cv::Mat img = cv::imread(imagePath);
 
-    cv::Mat grayImage;
-    cv::cvtColor(originalImage, grayImage, cv::COLOR_BGR2GRAY);
+    // Chuyển đổi ảnh sang không gian màu YUV
+    cv::Mat img_yuv;
+    cv::cvtColor(img, img_yuv, cv::COLOR_BGR2YUV);
 
-    cv::Mat balancedImage;
-    cv::equalizeHist(grayImage, balancedImage);
+    // Tạo một bản sao của ảnh YUV để cân bằng histogram
+    cv::Mat img_yuv_equalized = img_yuv.clone();
 
-    cv::Mat comparisonImage;
-    cv::hconcat(grayImage, balancedImage, comparisonImage);
+    // Áp dụng cân bằng histogram cho kênh Y (độ sáng)
+    std::vector<cv::Mat> channels;
+    cv::split(img_yuv_equalized, channels);
+    
 
-    cv::imshow("Comparison", comparisonImage);
+    cv::equalizeHist(channels[0], channels[0]);
+
+    auto end = std::chrono::high_resolution_clock::now(); // Kết thúc đo thời gian
+    std::chrono::duration<double> elapsed = end - start; // Thời gian đã trôi qua
+
+    cv::merge(channels, img_yuv_equalized);
+
+    // Chuyển đổi ảnh YUV sau khi cân bằng histogram về RGB
+    cv::Mat equalized_img;
+    cv::cvtColor(img_yuv_equalized, equalized_img, cv::COLOR_YUV2BGR);
+
+    // Hiển thị ảnh gốc và ảnh sau khi cân bằng histogram
+    cv::imshow("Original Image", img);
+    cv::imshow("Equalized Image", equalized_img);
     cv::waitKey(0);
+    cv::destroyAllWindows();
+
+    // In ra thời gian xử lý
+    std::cout << "Thời gian xử lý: " << elapsed.count() << " giây" << std::endl;
 
     return 0;
 }
